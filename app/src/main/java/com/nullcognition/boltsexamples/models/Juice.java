712f2b35e7452bbc73624eec442f4f593019394a
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-07-23 13:55:17 -0400

    minor changes - DrinkableLiquid is no longer a raw type, but takes the type parameter F which extends Flavour, thus allowing me  to remove the cast
    See - http://www.coderanch.com/t/652818/java/java/Object-Oriented-Design-Generics#3018481
    for todo's

--- app/src/main/java/com/nullcognition/boltsexamples/models/Juice.java
+++ app/src/main/java/com/nullcognition/boltsexamples/models/Juice.java
@@ -1,13 +1,13 @@
 package com.nullcognition.boltsexamples.models;// Created by ersin on 20/07/15
 
-public final class Juice<F extends Flavour> extends DrinkableLiquid{
+public final class Juice<F extends Flavour> extends DrinkableLiquid<F>{
 
 	public Juice(double amount, F flavour){
 		super(amount, flavour); //, nonAlcoholic);
 	}
 
 	public Wine<F> ferment(){
-		return new Wine<>(this.amount, (F) this.getFlavour());
+		return new Wine<>(this.amount, this.getFlavour());
 	}
 
 }
--- app/src/main/java/com/nullcognition/boltsexamples/models/Wine.java
+++ app/src/main/java/com/nullcognition/boltsexamples/models/Wine.java
@@ -1,6 +1,6 @@
 package com.nullcognition.boltsexamples.models;// Created by ersin on 16/07/15
 
-public final class Wine<F extends Flavour> extends DrinkableLiquid{
+public final class Wine<F extends Flavour> extends DrinkableLiquid<F>{
 
 	public Wine(double amount, F flavour){
 		super(amount, flavour);//, ofThisYear);
